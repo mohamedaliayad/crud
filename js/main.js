4613fa71673864b04f75ea15bb28364bead4274d
mohamedaliayad <mohamedalyayad888@gmail.com>
2023-07-28 06:40:31 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,62 +1,95 @@
-var bookmarkName=document.getElementById("bookmarkName")
-var bookmarkURLInput=document.getElementById("bookmarkURL")
-var bookmarks=[];
 
-bookmarks=JSON.parse(localStorage.getItem("links"));
-display()
-function submitData(){
-    var links ={
-        Name:bookmarkName.value,
-URL:bookmarkURLInput.value,
-     }
-bookmarks.push(links)
-localStorage.setItem("links" , JSON.stringify(bookmarks))
-display()
+var signupName = document.getElementById('signupName')
+var signupEmail = document.getElementById('signupEmail')
+var signupPassword = document.getElementById('signupPassword')
+var signinEmail = document.getElementById('signinEmail')
+var signinPassword = document.getElementById('signinPassword')
+var signUpdata=[];
 
-allLetter(bookmarkName)
+/////////////
+
+
+
+// sign-up/
+if (localStorage.getItem('users') == null) {
+    signUpdata = []
+} else {
+    signUpdata = JSON.parse(localStorage.getItem('users'))
+}
+function check(){
+    for(var i=0 ;i <signUpdata.length; i++ ){
+         if (signUpdata[i].email.toLowerCase() == signupEmail.value.toLowerCase()){
+           return false
+         }
+
+
+    }
+}
+function signUp() {
+
+    if (signupName.value == "" || signupEmail.value == "" || signupPassword.value == "") {
+        document.getElementById('exist').innerHTML = '<span class="text-danger m-3">All inputs is required</span>'
+   return false  }
+    var sign = {
+        name: signupName.value,
+        email: signupEmail.value,
+        password: signupPassword.value,
+    }
+
+
+if (signUpdata.length==0){
+    signUpdata.push(sign)
+    localStorage.setItem("users" , JSON.stringify(signUpdata))
+     document.getElementById('exist').innerHTML ='<span class="text-success m-3">Success</span>'
+return true}
+
+if (check() == false) {
+    document.getElementById('exist').innerHTML = '<span class="text-danger m-3">email already exists</span>'
 
 }
+else {
+    signUpdata.push(sign)
+    localStorage.setItem('users', JSON.stringify(signupdata))
+    document.getElementById('exist').innerHTML = '<span class="text-success m-3">Success</span>'
 
-function allLetter(bookmarkName)
-{
-var letters = /^[A-Za-z]+$/;
-if(bookmarkName.value.match(letters))
-{
-
-}
-else
-{
-
-windowBox.classList.remove("d-none")
-exitLayer.classList.remove("d-none")
 }
 }
 
+// signin///
 
+function LoginEmpty() {
 
-function display(){
-    var cartona=''
-for(var i=0; i<bookmarks.length; i++){
-    cartona+=`  <tr>
-    <td>${i}</td>
-    <td>${bookmarks[i].Name}</td>
-    <td>
-              <a href="https://${bookmarks[i].URL}" target="_blank" class="btn btn-visit pe-2"> <i class="fa-solid fa-eye pe-2"></i>Visit</a
-             </td>
-    <td>
-      <button class="btn btn-delete pe-2" onclick="Delete(${i})" >
-        <i class="fa-solid fa-trash-can" ></i>
-        Delete
-      </button>
-    </td>
-</tr>`
-}
-document.getElementById("tableData").innerHTML=cartona
+    if (signinPassword.value == "" || signinEmail.value == "") {
+        return false
+    } else {
+        return true
+    }
 }
 
-function Delete(x){
-bookmarks.splice(x , 1)
-localStorage.setItem("links" , JSON.stringify(bookmarks))
-display()
-}
+function login() {
+    if (LoginEmpty() == false) {
+        document.getElementById('incorrect').innerHTML = '<span class="text-danger m-3">All inputs is required</span>'
+        return false
+    }
+    var password = signinPassword.value
+    var email = signinEmail.value
+    for (var i = 0; i < signUpdata.length; i++) {
+        if (signUpdata[i].email.toLowerCase() == email.toLowerCase() && signUpdata[i].password.toLowerCase() == password.toLowerCase()) {
+            localStorage.setItem('sessionUsername', signUpdata[i].name)
+
+    window.location.href = "home.html";
+        } else {
+            document.getElementById('incorrect').innerHTML = '<span class="p-2 text-danger">incorrect email or password</span>'
+        }
+            }
+
+        }
+//home////
+
+        var username = localStorage.getItem('sessionUsername')
+        if (username) {
+            document.getElementById("welcome").innerHTML = "Welcome " + username
+        }
+
+
 
